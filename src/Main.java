3e4a1227b078fe663a79b26891fd9b752f33fe80
MEHUL KUMAR <mehul@MEHULs-MacBook-Pro.local>
2022-09-03 14:24:36 +0530

    jenkins final day assesment

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition");
+        System.out.println("sum of the numbers : "+ (a+b));
+
+        System.out.println("Subtraction");
+        System.out.println("subtraction of the numbers : "+ (a-b));
+
+        System.out.println("Multiplication");
+        System.out.println("Multiplication of the numbers : "+ a*b);
+
+        System.out.println("Division");
+        System.out.println("Division of the numbers : "+ a/b);
+    }
+}
